39399bb16b93f1a87cb331b0ee72bb54e30373a0
git_pro_afzal <mohamedafzal.softsuave@gmail.com>
2022-08-06 12:25:36 +0530

    Added few print statements in the Test.java file

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ public class Test {
 
     public static void main(String[] args) {
         
-        System.out.println("Welcome to Git!!");
+        System.out.println("Welcome to Git!! This is our First Project.");
     }
 }
